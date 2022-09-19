1173469c1bf593bb8f2c77ca60a1f55b02acd2fb
bwain <bwain@grubhub.com>
2022-09-19 18:00:37 -0500

    break app

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: { fullySpecified: false },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.m?js$/,
+      type: 'javascript/auto',
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
